80e9195da86c12f403ae5acc6820cfbdb4904cb3
sarwesh <117828172+Tech-sarwesh@users.noreply.github.com>
2026-01-02 20:54:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Friend!  This is Azure CI-CD Demo App Changes 1."
+    return "Hello Friend!  This is Azure CI-CD Demo App Changes 2."
 
 if __name__ == "__main__":
     app.run()
+
 
